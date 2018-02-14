ee704c6efc0f138b8f9215b4cd924c57336a8f45
Danielle Moit <danielle.e.moit@gmail.com>
2018-02-13 22:23:38 -0500

    Add files via upload

---
+++ assets/js/todos.js
@@ -0,0 +1,28 @@
+//Check off Todos by clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+//Click on X to delete todo
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+//add new todo
+$("input[type = 'text']").keypress(function(event){
+	if(event.which === 13){
+		//grab new todotext
+		var todoText = $(this).val();
+		$(this).val("");
+		//create new li
+		$("ul").append("<li><span><i class='fa fa-trash' aria-hidden='true'></i></span> " + todoText + "</li>");
+
+	}
+});
+
+$(".fa-plus").click(function(){
+	$("input[type = 'text']").fadeToggle();
+});
